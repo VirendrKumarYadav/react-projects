629451255248b3e4b76de682c4558aaf2b95dcbe
virendra <softech.vire@gmail.com>
2024-01-08 13:51:25 +0530

    adding the data

---
+++ text-translater/src/App.js
@@ -0,0 +1,57 @@
+import React, {useState} from "react";
+import axios from "axios";
+import Footer from "./componant/Footer copy/Footer";
+import Header from "./componant/Header copy/Header";
+import Section from "./componant/Section/Section";
+
+const App = () => {
+const [targetText,setTargetText] = useState("")
+const [sourceText, setSourceText] = useState(" howggg");
+
+  const [source,setSource] = useState("en");
+   const [target,setTarget] = useState("id");
+
+
+  const postApi = async () => {
+    const url = "https://text-translator2.p.rapidapi.com/translate";
+    const apikey = "a87ea0f1a7mshab123e16f05bfffp18c53ajsnf346c549d3a4";
+
+    const encodedParams = new URLSearchParams();
+    encodedParams.set("source_language", source);
+    encodedParams.set("target_language", target);
+    encodedParams.set("text", sourceText);
+
+    const options = {
+      method: "POST",
+      url: url,
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": apikey,
+        "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
+      },
+      data: encodedParams,
+    };
+
+    try {
+      const response = await axios.request(options);
+      setTargetText(response.data.data.translatedText);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+ const convertToTargetLang = () => {
+   postApi();
+}
+
+
+  return (
+    <div  className="h-screen w-screen flex flex-col justify-between">
+      <Header />
+      <Section sourceText={(e) => (setSourceText(e))} targetText={targetText} setSouceLanuage={(e) => (setSource(e))} setTargetLanuage={(e) => (setTarget(e))} convertToTargetLang={convertToTargetLang} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App
---
+++ text-translater/src/componant/Header copy/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+
+  const filterImage = (e) => {
+  console.log(e.terget.value);
+  }
+
+  return (
+    <div className='font-serif flex flex-row justify-between px-3 py-2 bg-gray-300 items-center' >
+          <div>
+              <span>Rishi Translator</span>
+      </div>
+      
+          <div className='flex gap-3'>
+              <input placeholder='Search...' className='px-3 rounded-xl' onChange={(e)=>filterImage(e)}></input>
+              <button className='rounded-md px-4 py-2 border-green-400 border-2 hover:bg-green-400 '>Search</button>
+          </div>
+    </div>
+  )
+}
+
+export default Header
