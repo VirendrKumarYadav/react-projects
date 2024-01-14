3af73ef9fdcfb73049580d113cc281f50bd24351
virendra <softech.vire@gmail.com>
2024-01-14 17:26:47 +0530

    adding data

--- weaktest_4/src/compo/RegisterList.js
+++ weaktest_4/src/compo/RegisterList.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from "react";
 const DataList = () => {
- const [myObject, setMyObject] = useState(null);
+  const [myObject, setMyObject] = useState(JSON.parse(localStorage.getItem("regitered")));
 
+  useEffect(() => {
+    const storedString = localStorage.getItem("regitered");
+    if (storedString) {
+      const parsedObject = JSON.parse(storedString);
+      setMyObject(parsedObject);
+    }
+  }, []);
 
- useEffect(() => {
-   const storedString = localStorage.getItem("regitered");
-   if (storedString) {
-     const parsedObject = JSON.parse(storedString);
-     setMyObject(parsedObject);
-   }
- }, []);
- 
   return (
     <div>
       <h1 className="bg-cyan-800 py-2 px-3 text-white font-serif font-bold text-center rounded-sm shadow-2xl">
@@ -18,15 +17,20 @@ const DataList = () => {
       </h1>
       <div>
         {myObject == null ? (
-          "hello there is no Datalist "
+          " hello there is no Datalist "
         ) : (
-            <div> "mapping obj data"+{myObject.map((data,idx) => {
-              <p>{data.name}</p> 
-          })}</div>
+          <div className="flex flex-row justify-between font-serif text-xl py-1">
+            <p>Name :-{myObject.name}</p>
+            <p>Age:-{myObject.age}</p>
+            <p>Adhar :- {myObject.adhar}</p>
+            <p>Mobile No :-{myObject.mobile}</p>
+            <p>DOB :-{myObject.dob}</p>
+          </div>
         )}
+        
       </div>
     </div>
   );
-}
+};
 
-export default DataList
+export default DataList;
